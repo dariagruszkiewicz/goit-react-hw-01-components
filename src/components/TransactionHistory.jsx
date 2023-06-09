f2a3e62d0ba2273eae6ab605477f141f19ea2369
Daria Gruszkiewicz <dariaa.pajak@gmail.com>
2023-06-09 21:47:06 +0200

    update hw-01-components
    update hw-01-components

---
+++ src/components/TransactionHistory.jsx
@@ -0,0 +1,29 @@
+import css from './TransactionHistory.module.css';
+import { TransactionHistoryItem } from './TransactionHistoryItem';
+
+export const TransactionHistory = ({ items }) => {
+  return (
+    <table className={css.transaction_history}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(item => {
+          return (
+            <TransactionHistoryItem
+              key={item.id}
+              type={item.type}
+              amount={item.amount}
+              currency={item.currency}
+            />
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
